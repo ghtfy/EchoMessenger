550c79a04356a7beacfa741677701b6a3a657ef4
keunwoo kim <keun1999@outlook.kr>
2026-03-19 10:55:14 +0900

    과제1 의 테스트 완료 Label을 상단에 배치하여 메시지박스의 제목부분을 배치. ListBox를 중앙에 배치하며 TextBox를 그 아래에 배치함. 각각의 컨트롤을 배치하며 메시지창의 기본 UI 배치를 해두었습니다.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,73 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("굴림", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(378, 44);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Echo Messenger";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(20, 373);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(581, 25);
+            this.textBox1.TabIndex = 1;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(20, 73);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(581, 274);
+            this.listBox1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.Info;
+            this.button1.Font = new System.Drawing.Font("한컴 고딕", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.button1.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.button1.Location = new System.Drawing.Point(631, 345);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(143, 76);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "전송";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -15,6 +15,23 @@ namespace EchoMessenger
         public Form1()
         {
             InitializeComponent();
+            // button1의 Click 이벤트 핸들러 등록
+            button1.Click += Button1_Click;
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            // TextBox에 입력된 내용 가져오기
+            string typed_msg = textBox1.Text;
+            
+            // ListBox에 항목 추가
+            listBox1.Items.Add(typed_msg);
+            
+            // TextBox 비우기 (선택사항이지만 좋은 UX)
+            textBox1.Text = "";
+            
+            // 커서를 TextBox로 돌리기 (선택사항)
+            textBox1.Focus();
         }
     }
 }
