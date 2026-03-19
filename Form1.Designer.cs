4163cb8ae2723acba2f4b3e257751e2db09cc53c
keunwoo kim <keun1999@outlook.kr>
2026-03-19 11:35:53 +0900

    과제1 코딩과 테스트 완료. 사용자가 입력한 메시지를 ListBox에 추가하여 출력하고, 전송 버튼 클릭 시 TextBox내 내용을 초기화 하고 새로운 테스트를 받도록 초기화하고 유지함.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -41,24 +41,25 @@
             this.label1.ForeColor = System.Drawing.SystemColors.Highlight;
             this.label1.Location = new System.Drawing.Point(12, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(378, 44);
+            this.label1.Size = new System.Drawing.Size(368, 43);
             this.label1.TabIndex = 0;
             this.label1.Text = "Echo Messenger";
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(20, 373);
+            this.textBox1.Location = new System.Drawing.Point(20, 381);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(581, 25);
             this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // listBox1
             // 
             this.listBox1.FormattingEnabled = true;
             this.listBox1.ItemHeight = 15;
-            this.listBox1.Location = new System.Drawing.Point(20, 73);
+            this.listBox1.Location = new System.Drawing.Point(20, 65);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(581, 274);
+            this.listBox1.Size = new System.Drawing.Size(736, 274);
             this.listBox1.TabIndex = 2;
             // 
             // button1
@@ -66,9 +67,9 @@
             this.button1.BackColor = System.Drawing.SystemColors.Info;
             this.button1.Font = new System.Drawing.Font("한컴 고딕", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
             this.button1.ForeColor = System.Drawing.SystemColors.MenuHighlight;
-            this.button1.Location = new System.Drawing.Point(631, 345);
+            this.button1.Location = new System.Drawing.Point(621, 361);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(143, 76);
+            this.button1.Size = new System.Drawing.Size(135, 68);
             this.button1.TabIndex = 3;
             this.button1.Text = "전송";
             this.button1.UseVisualStyleBackColor = false;
--- Form1.cs
+++ Form1.cs
@@ -15,23 +15,25 @@ namespace EchoMessenger
         public Form1()
         {
             InitializeComponent();
-            // button1의 Click 이벤트 핸들러 등록
-            button1.Click += Button1_Click;
+            
+            button1.Click += Button1_Click; // button1의 Click 이벤트 핸들러 등록
         }
 
         private void Button1_Click(object sender, EventArgs e)
         {
-            // TextBox에 입력된 내용 가져오기
-            string typed_msg = textBox1.Text;
             
-            // ListBox에 항목 추가
-            listBox1.Items.Add(typed_msg);
+            string typed_msg = textBox1.Text; // TextBox에 입력된 내용 가져오기
+
+            listBox1.Items.Add(typed_msg); // ListBox에 항목 추가
+
+            textBox1.Text = "";// TextBox 비우기 (선택사항이지만 좋은 UX)
+           
+            textBox1.Focus(); // 커서를 TextBox로 돌리기 (선택사항)
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
             
-            // TextBox 비우기 (선택사항이지만 좋은 UX)
-            textBox1.Text = "";
-            
-            // 커서를 TextBox로 돌리기 (선택사항)
-            textBox1.Focus();
         }
     }
 }
