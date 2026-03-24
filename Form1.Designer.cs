b3fe5ff7ad48c490c5fd3ef5df080edda3984de2
keunwoo kim <keun1999@outlook.kr>
2026-03-24 22:17:29 +0900

    Form1.cs 디자인 UI 디자인 수정

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -40,43 +40,47 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("굴림", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.label1.ForeColor = System.Drawing.SystemColors.Highlight;
-            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Font = new System.Drawing.Font("맑은 고딕", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(90)))), ((int)(((byte)(160)))));
+            this.label1.Location = new System.Drawing.Point(20, 16);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(368, 43);
+            this.label1.Size = new System.Drawing.Size(289, 46);
             this.label1.TabIndex = 0;
             this.label1.Text = "Echo Messenger";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // TextMessage
             // 
-            this.TextMessage.Font = new System.Drawing.Font("굴림", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.TextMessage.Location = new System.Drawing.Point(24, 373);
+            this.TextMessage.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.TextMessage.Font = new System.Drawing.Font("맑은 고딕", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.TextMessage.Location = new System.Drawing.Point(28, 398);
             this.TextMessage.Name = "TextMessage";
-            this.TextMessage.Size = new System.Drawing.Size(581, 42);
+            this.TextMessage.Size = new System.Drawing.Size(589, 39);
             this.TextMessage.TabIndex = 1;
             this.TextMessage.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // EchoMessageList
             // 
-            this.EchoMessageList.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.EchoMessageList.BackColor = System.Drawing.Color.White;
+            this.EchoMessageList.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.EchoMessageList.Font = new System.Drawing.Font("맑은 고딕", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
             this.EchoMessageList.FormattingEnabled = true;
-            this.EchoMessageList.ItemHeight = 15;
-            this.EchoMessageList.Location = new System.Drawing.Point(20, 65);
+            this.EchoMessageList.ItemHeight = 23;
+            this.EchoMessageList.Location = new System.Drawing.Point(28, 78);
             this.EchoMessageList.Name = "EchoMessageList";
-            this.EchoMessageList.Size = new System.Drawing.Size(736, 274);
+            this.EchoMessageList.Size = new System.Drawing.Size(732, 255);
             this.EchoMessageList.TabIndex = 2;
             this.EchoMessageList.SelectedIndexChanged += new System.EventHandler(this.EchoMessageList_SelectedIndexChanged);
             // 
             // button1
             // 
-            this.button1.BackColor = System.Drawing.SystemColors.Info;
-            this.button1.Font = new System.Drawing.Font("한컴 고딕", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.button1.ForeColor = System.Drawing.SystemColors.MenuHighlight;
-            this.button1.Location = new System.Drawing.Point(621, 361);
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(152)))), ((int)(((byte)(219)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("맑은 고딕", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(630, 390);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(135, 68);
+            this.button1.Size = new System.Drawing.Size(130, 47);
             this.button1.TabIndex = 3;
             this.button1.Text = "전송";
             this.button1.UseVisualStyleBackColor = false;
@@ -84,40 +88,45 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("굴림", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.label2.Location = new System.Drawing.Point(20, 350);
+            this.label2.Font = new System.Drawing.Font("맑은 고딕", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label2.ForeColor = System.Drawing.Color.DimGray;
+            this.label2.Location = new System.Drawing.Point(24, 357);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(134, 20);
+            this.label2.Size = new System.Drawing.Size(114, 23);
             this.label2.TabIndex = 4;
             this.label2.Text = "현재대화: 0개";
             // 
             // btnDelete
             // 
-            this.btnDelete.BackColor = System.Drawing.SystemColors.ControlLight;
-            this.btnDelete.Font = new System.Drawing.Font("한컴 고딕", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.btnDelete.Location = new System.Drawing.Point(621, 301);
+            this.btnDelete.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.btnDelete.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDelete.Font = new System.Drawing.Font("맑은 고딕", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btnDelete.Location = new System.Drawing.Point(630, 287);
             this.btnDelete.Name = "btnDelete";
-            this.btnDelete.Size = new System.Drawing.Size(135, 40);
+            this.btnDelete.Size = new System.Drawing.Size(130, 40);
             this.btnDelete.TabIndex = 5;
             this.btnDelete.Text = "삭제";
             this.btnDelete.UseVisualStyleBackColor = false;
             // 
             // btnClearAll
             // 
-            this.btnClearAll.BackColor = System.Drawing.SystemColors.ControlLight;
-            this.btnClearAll.Font = new System.Drawing.Font("한컴 고딕", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.btnClearAll.Location = new System.Drawing.Point(621, 260);
+            this.btnClearAll.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.btnClearAll.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnClearAll.Font = new System.Drawing.Font("맑은 고딕", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btnClearAll.Location = new System.Drawing.Point(630, 241);
             this.btnClearAll.Name = "btnClearAll";
-            this.btnClearAll.Size = new System.Drawing.Size(135, 40);
+            this.btnClearAll.Size = new System.Drawing.Size(130, 40);
             this.btnClearAll.TabIndex = 6;
             this.btnClearAll.Text = "대화기록삭제";
             this.btnClearAll.UseVisualStyleBackColor = false;
             // 
             // Form1
             // 
+            this.AcceptButton = this.button1;
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(245)))), ((int)(((byte)(247)))), ((int)(((byte)(250)))));
+            this.ClientSize = new System.Drawing.Size(800, 460);
             this.Controls.Add(this.btnClearAll);
             this.Controls.Add(this.btnDelete);
             this.Controls.Add(this.button1);
@@ -125,8 +134,11 @@
             this.Controls.Add(this.EchoMessageList);
             this.Controls.Add(this.TextMessage);
             this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Echo Messenger";
             this.ResumeLayout(false);
             this.PerformLayout();
 
